2a1385fae14f90cc1341a8aacaad9e85bcd80a56
marcus0329 <skuan@dons.usfca.edu>
2023-10-06 20:59:11 -0700

    Added service

--- src/main/java/com/passwordgenerator/user/UserController.java
+++ src/main/java/com/passwordgenerator/user/UserController.java
@@ -1,6 +1,7 @@
 package com.passwordgenerator.user;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,11 +13,12 @@ import java.util.List;
 public class UserController {
 
 
-//    private  UserService userService;
+    private final UserService userService;
 
-    /*public UserController(UserService userService) {
+    @Autowired
+    public UserController(UserService userService) {
         this.userService = userService;
-    }*/
+    }
 
     @GetMapping
     public String hello() {
